344082ffb31528ec7d4d06f810caa97c2b98941a
Andrei Solntsev <andrei.solntsev@gmail.com>
2020-01-14 22:49:59 +0200

    try to upgrade to Selenium 4.0.0-alpha-4
    grid tests are failing... :(

--- statics/src/test/java/grid/AbstractGridTest.java
+++ statics/src/test/java/grid/AbstractGridTest.java
@@ -6,25 +6,21 @@ import com.codeborne.selenide.webdriver.WebDriverBinaryManager;
 import integration.IntegrationTest;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.grid.selenium.GridLauncherV3;
-import org.openqa.grid.shared.Stoppable;
+import org.openqa.selenium.grid.Main;
 
-import static com.codeborne.selenide.Selenide.close;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static org.openqa.selenium.net.PortProber.findFreePort;
 
 abstract class AbstractGridTest extends IntegrationTest {
-  private Stoppable gridHub;
-  private Stoppable gridNode;
   int hubPort;
 
   @BeforeEach
-  final void setUpGrid() {
-    close();
+  final void setUpGrid() throws Exception {
+    closeWebDriver();
 
     hubPort = findFreePort();
-    gridHub = new GridLauncherV3().launch(new String[]{"-port", "" + hubPort});
-
-    gridNode = new GridLauncherV3().launch(new String[]{"-port", "" + findFreePort(),
+    Main.main(new String[]{"standalone", "-port", "" + hubPort});
+    Main.main(new String[]{"node", "-port", "" + findFreePort(),
       "-role", "node",
       "-hub", "http://localhost:" + hubPort + "/grid/register"
     });
@@ -34,8 +30,6 @@ abstract class AbstractGridTest extends IntegrationTest {
 
   @AfterEach
   final void tearDownGrid() {
-    close();
-    gridHub.stop();
-    gridNode.stop();
+    closeWebDriver();
   }
 }
