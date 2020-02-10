052446e7b84eea90f144bdc8a20904e6a79c532b
Thomas <60699205+thodah-kcb@users.noreply.github.com>
2020-02-10 15:15:53 +0100

    Create poc-powershell.ps1

---
+++ poc-powershell.ps1
@@ -0,0 +1,37 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+Function Get-Token
+{
+    [CmdletBinding()]
+
+    $body = @{
+        client_id = $Env.auth0_clientid
+        client_secret = $Env.auth0_clientSecret
+        grant_type = "client_credentials"
+        audience = "https://dev-iqbfcaun.eu.auth0.com/api/v2/"
+    };
+
+    $headers = @{
+        "content-type" = "application/json")
+    };
+
+    try
+    {
+        $response = Invoke-RestMethod "$Env.auth0_url" -Method Post -Body $body -Headers $headers;
+        $token = $response.access_token;
+        
+        echo $token
+        
+        return $token;
+    }
+    catch
+    {
+        $result = $_.Exception.Response.GetResponseStream();
+        $reader = New-Object System.IO.StreamReader($result);
+        $reader.BaseStream.Position = 0;
+        $reader.DiscardBufferedData();
+        $responseBody = $reader.ReadToEnd() | ConvertFrom-Json
+        Write-Host "ERROR: $($responseBody.error)"
+        return;
+    }
+}
